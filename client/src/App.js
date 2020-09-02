cb247212241c0e3d98f02c96223ba86b0573899f
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-02 15:14:16 +1000

    made a simple login form using the get user functionality of the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './App.css';
 import Axios from "axios";
+import { Form, Button, Image, Col, Row } from "react-bootstrap";
 
 class App extends React.Component {
     state = {
-        backend: "did not connect to backend :("
+        backend: "did not connect to backend :(",
+        email: "",
+        password: "",
+        response: {empty: "fake"}
     };
+
+    onChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        const payload = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        Axios.post("api/user/test", payload).then((resp) => {
+            this.setState({response: resp.data});
+        }).catch((err) => {
+            console.error(err);
+        });
+
+    }
 
     componentDidMount() {
 
@@ -26,6 +50,45 @@ class App extends React.Component {
                 </p>
                 <p>
                     {this.state.backend}
+                </p>
+                <Form className="loginForm" onSubmit={this.onSubmit}>
+
+                    <Form.Group as={Col} xs="12" controlId="Email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control
+                            required
+                            type="email"
+                            placeholder="example@example.com"
+                            name="email"
+                            onChange={this.onChange}
+                        />
+                    </Form.Group>
+                    <Form.Group as={Col} xs="12" controlId="Password">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            required
+                            type="password"
+                            placeholder="Enter your Password"
+                            name="password"
+                            onChange={this.onChange}
+                        />
+
+                    </Form.Group>
+                    <div className="row justify-content-center">
+                        <Button
+                            variant="success"
+                            size="lg"
+                            type="submit"
+                            block
+                            style={{ width: "90%", margin: "1.6rem auto" }}
+                        >
+                            Log in
+                        </Button>
+                    </div>
+                </Form>
+                <p>
+                    this.state.response
+                    {JSON.stringify(this.state.response)}
                 </p>
             </div>
         );

fa754e7e31513a958dc8fcc02e96bdb4b7e48021
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-07 19:03:02 +1000

    created an env function which get env variables from the correct file

---
+++ utils/env.js
@@ -0,0 +1,10 @@
+const setEnv = () => {
+    if (process.env.NODE_ENV === 'development') {
+        require('dotenv').config({path:__dirname+"/./../.env.development"});
+    }
+    else if (process.env.NODE_ENV === 'test') {
+        require('dotenv').config({path:__dirname+"/./../.env.test"});
+    }
+}
+
+module.exports.setEnv = setEnv;

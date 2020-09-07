eba4e9f7e244ce7055fcf947745d1e026df87b35
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-07 19:05:13 +1000

    created a simple integration test

--- test/unit/myAdd.test.js
+++ test/unit/myAdd.test.js
@@ -1,5 +1,28 @@
-const users = require('../../controllers/user');
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(users.myAdd(1, 2)).toBe(3);
+//const users = require('../../controllers/user');
+const request = require("supertest");
+const app = require("../../app");
+const database = require("../../models/index");
+describe("my first test", ()=> {
+    beforeEach(() => {
+        return database.clearCollections();
+    });
+    test("tests first function", async (done) => {
+        const payload = {
+            email: "test@123245.com",
+            password: "testing123",
+            firstName: "firstTest",
+            lastName: "lastTest",
+            username: "user22Test"
+        }
+        const response = await request(app)
+            .post("/api/user/add")
+            .send(payload)
+        expect(response.status).toBe(200);
+        done();
+    });
 });
+
+afterAll(async (done) => {
+    database.disconnectDB();
+    done();
+})

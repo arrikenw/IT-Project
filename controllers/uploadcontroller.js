30ce25955b0a4eda888f4afbdb59ffa6bec243a2
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-09-08 09:46:04 +1000

    Update media upload skeleton
    +Added logic for responses to client
    +Improved control flow and code structure

--- controllers/uploadcontroller.js
+++ controllers/uploadcontroller.js
@@ -10,51 +10,58 @@ const User = mongoose.model('users');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 
-// Bucket data
+//send helper
+const sendhelper = function(res, response){
+    res.status = response.status;
+    res.send(response.msg);
+}
+
+
+//put info about uploaded file into the database
+const dbSaveMetadata = function(file, fields, user, resourceurl){
+    const item = {
+        mediaType: file.type.split('/')[0],
+        mediaURL: resourceurl,
+        tags: fields.tags,
+        creator: user._id,
+        isPrivate: fields.isPrivate,
+        canAccess: fields.canAccess
+    }
+    const newmedia = new Media(item);
+    newmedia.save()
+        .then(function (media, err){
+            if (err){
+                console.log("saving to db failed")
+                sendhelper({status:503, msg:err});
+            }
+            else{
+                console.log(media);
+                sendhelper({status:200, msg:media});
+            }
+        });
+}
+
 
 const uploadMedia = function(file, fields, user){
-    //bucket data
+    //information about bucket and upload
     const bucketName = 'it-project-media';
-    const keyName = user + '/' + file.name;
+    const keyName = user + '/' + file.name; //TODO check for key collisions
+    const baseurl = 'https://it-project-media.s3-ap-southeast-2.amazonaws.com/'
+
+    //actual resource url
+    const resourceurl = baseurl + keyName;
 
     //writing to bucket
     const params = {Bucket: bucketName, Key: keyName, Body: file.body}; //idk if it is file.body, file.data etc.
     s3.putObject(params, function(err, data) {
-
         //check upload success
-        if (err) return err
-        console.log("Successfully uploaded data to " + bucketName + '/' + keyName);
-
-        //put info about uploaded file into the database
-        const item = {
-            mediaType: file.type.split('/')[0],
-            mediaURL: data.TODO, //TODO FIGURE OUT WHAT THE BUCKET RETURNS
-            tags: fields.tags,
-            creator: user._id,
-            isPrivate: fields.isPrivate,
-            canAccess: fields.canAccess
+        if (err){
+            console.log("Upload to bucket failed");
+            sendhelper({status:503, msg:"upload failed"});
         }
-        console.log(item);
-        const newmedia =new Media(item);
-        newmedia.save()
-            .then(function (media, err){
-                if (err){
-                    //TODO SEND TO USER
-                    return err;
-                }
-                else{
-                    //TODO SEND TO USER
-                    console.log(media);
-                }
-            })
-            .catch(function (err){
-                //TODO SEND TO USER
-                //TODO HANDLE DB ISSUES
-                return err;
-            });
-        console.log(data);
+        console.log("Successfully uploaded data to " + bucketName + '/' + keyName);
+        dbSaveMetadata(file, fields, user, resourceurl);
     });
-    return 'SUCCESS';
 }
 
 
@@ -80,38 +87,37 @@ const validateFields = function(fields){
 
 const validateAll = function(file){
     if (!validateMediaSize(file)){
-        return 'ERROR. File was too large';
+        return {status: 400, msg:'ERROR. File was too large'};
     }
     if (!validateMediaType(file)){
-        return 'ERROR. File type is unsupported';
+        return {status: 415, msg:'ERROR. File type is unsupported'};
     }
     fieldStatus = validateFields(fields);
     return fieldStatus;
 }
 
-const handleMediaUpload = (req) => {
+const handleMediaUpload = function(req, res){
     const form = new formidable.IncomingForm();
     form.maxFileSize = 15 * 1024 * 1024; //15 meg
     form.parse(req, (err, fields, files) => {
         if (err) {
             console.log(err);
-            return false;
+            sendhelper(res,{status:500, msg:"ERROR. Form data could not be parsed"});
+            return;
         }
         const validationStatus = validateAll(files[0], fields);
         if (validationStatus != 'SUCCESS'){
-            //TODO send message to user
-            return validationStatus;
+            console.log(validationStatus);
+            sendhelper(validationStatus);
+            return;
         }
         console.log('validation success');
-        const uploadStatus = uploadMedia(file, fields, user);
-        if (uploadStatus != 'SUCCESS'){
-            //TODO send message to user
-            return uploadStatus;
-        }
+        uploadMedia(file, fields, user);
     })
     .catch('error', function (err){
-            console.log(err)
+            console.log(err);
+            res.status = 500;
+            res.send("Unknown error when parsing form");
     });
-    return;
 }
 

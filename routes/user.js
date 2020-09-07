e517edc18ed1df2ad05c3138914075528a120797
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-07 19:03:36 +1000

    added all routes for users

--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
-
+const { generateToken, authenticateToken } = require("../utils/jwtTokens");
 const userController = require('../controllers/user');
 
-router.post('/test', (req, res, next)=> {
+//get a user's details using their authentication token
+router.get('/get', authenticateToken, (req, res, next) => {
     userController.getUser(req, res, next);
 });
 
-router.post('/add', (req, res, next)=> {
+//get a list of user's public details
+router.post('/getPublic', (req, res, next) => {
+    userController.getPublicUser(req, res, next);
+});
+
+//add a new user to the database
+router.post('/add', (req, res, next) => {
     userController.addUser(req, res, next);
+});
+
+//get a valid authentication token from a user's login details
+router.post('/login', (req, res, next) => {
+    userController.loginUser(req, res, next);
+});
+
+//update a user's details using their authentication token and password
+router.post('/update', authenticateToken, (req, res, next) => {
+    userController.updateUser(req, res, next);
+});
+
+//delete a user's details using their authentication token and password
+router.post('/delete', authenticateToken, (req, res, next) => {
+    userController.deleteUser(req, res, next);
 });
 
 

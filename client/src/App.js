fb0e30b5ade90b075ebf713526bc2f3997b621d2
Daniel Jiang <49257039+danieljiang2013@users.noreply.github.com>
2020-09-09 18:05:14 +1000

    added upload component to app.js routing

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Form, Button, Image, Col, Row } from "react-bootstrap";
 import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 import Login from "./components/Login/Login";
 import Signup from "./components/Signup/Signup";
+import Upload from "./components/Upload/Upload";
 
 class App extends React.Component {
     state = {

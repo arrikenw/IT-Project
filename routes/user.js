29ea2db0d211e362249460056b139d04d5b1c22a
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-09 13:42:21 +1000

    finished routing and controllers for users

--- routes/user.js
+++ routes/user.js
@@ -1,38 +1,38 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
-const { generateToken, authenticateToken } = require("../utils/jwtTokens");
-const userController = require('../controllers/user');
+const { authenticateToken } = require("../utils/jwtTokens");
+const userController = require("../controllers/user");
 
-//get a user's details using their authentication token
-router.get('/get', authenticateToken, (req, res, next) => {
-    userController.getUser(req, res, next);
+// get a user's details using their authentication token
+router.get("/get", authenticateToken, (req, res) => {
+  userController.getUser(req, res);
 });
 
-//get a list of user's public details
-router.post('/getPublic', (req, res, next) => {
-    userController.getPublicUser(req, res, next);
+// get a list of user's public details
+router.post("/getPublic", (req, res) => {
+  userController.getPublicUser(req, res);
 });
 
-//add a new user to the database
-router.post('/add', (req, res, next) => {
-    userController.addUser(req, res, next);
+// add a new user to the database
+router.post("/add", (req, res) => {
+  userController.addUser(req, res);
 });
 
-//get a valid authentication token from a user's login details
-router.post('/login', (req, res, next) => {
-    userController.loginUser(req, res, next);
+// get a valid authentication token from a user's login details
+router.post("/login", (req, res) => {
+  userController.loginUser(req, res);
 });
 
-//update a user's details using their authentication token and password
-router.post('/update', authenticateToken, (req, res, next) => {
-    userController.updateUser(req, res, next);
+// update a user's details using their authentication token and password
+router.post("/update", authenticateToken, (req, res) => {
+  userController.updateUser(req, res);
 });
 
-//delete a user's details using their authentication token and password
-router.post('/delete', authenticateToken, (req, res, next) => {
-    userController.deleteUser(req, res, next);
+// delete a user's details using their authentication token and password
+router.post("/delete", authenticateToken, (req, res) => {
+  userController.deleteUser(req, res);
 });
 
-
-//export router for use index.js
+// export router for use index.js
 module.exports = router;

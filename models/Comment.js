bd9b0eb368cf22b32c6b30f33d03b5059e8c4ad6
arrikenw <arrikenw@student.unimelb.edu.au>
2020-09-24 02:10:56 +1000

    created schema for comments

---
+++ models/Comment.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// schema for comments
+const commentSchema = new Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "users",
+    },
+    body: { type: String, maxLength: 2200 },
+    likedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: "users" }],
+  },
+  { timestamps: true, collection: "Comments" }
+);
+
+// export model
+module.exports = commentSchema;

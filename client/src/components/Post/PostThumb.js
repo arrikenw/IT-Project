bd7db6fd0ba07232403768a520b9ee4681af3f9c
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-10-01 02:43:06 +1000

    Updated backend to reflect new post structure
    +Updated backend to reflect new post structure

--- client/src/components/Post/PostThumb.js
+++ client/src/components/Post/PostThumb.js
@@ -11,36 +11,7 @@ class PostThumb extends React.Component {
     };
     constructor (props) {
         super(props);
-        this.thumbHelper = this.thumbHelper.bind(this);
     }
-
-    //TODO  update backend to support this
-    thumbHelper = () => {
-        let videoThumb = "5f736f876b93c95300af61e0";
-        let audioThumb = "5f736fae6b93c95300af61e1";
-        let imageThumb = "5f736fdd6b93c95300af61e2";
-        let textThumb = "5f7370026b93c95300af61e3";
-        let applicationThumb = "5f7370156b93c95300af61e4";
-        console.log("init: "+this.props.post.thumbnailURL);
-        if (!this.props.post.thumbnailURL) {
-            switch (this.props.post.contentCategory) {
-                case "video":
-                    return videoThumb;
-                case "image":
-                    return imageThumb;
-                case "audio":
-                    return audioThumb;
-                case "text":
-                    return textThumb;
-                case "application":
-                    return applicationThumb;
-            }
-        }else{
-            return this.props.post.thumbnailURL
-        }
-    }
-
-
 
     render(){
         return(
--- controllers/post.js
+++ controllers/post.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const PostModel = mongoose.model("posts");
+const Media = mongoose.model("media");
 
 const getPost = (req, res) => {
   const user = req.user.id;
@@ -138,32 +139,20 @@ const getPublicPost = (req, res) => {
     .catch(onError);
 };
 
-const addPost = (req, res) => {
-  if (!req.body.title) {
-    console.log("addPost not successful: missing forum attributes");
-    res.status(400);
-    res.send("Add post not successful - missing forum attributes");
-    return;
-  }
 
-  const payload = { title: req.body.title, userID: req.user.id };
-  if (req.body.description) {
-    payload.description = req.body.description;
-  }
-  if (req.body.private) {
-    payload.private = req.body.private;
-  }
+//done and dusted :cool:
+const addPost = (req, res) => {
 
   const onPostSave = (err, newPost) => {
     // catch different errors
     if (err) {
       if (err.message.startsWith("error name")) {
         console.log(
-          "addPost not successful: email address already has an account"
+            "addPost not successful: email address already has an account"
         );
         res.status(400);
         res.send(
-          "Add Post not successful - email address already has an account"
+            "Add Post not successful - email address already has an account"
         );
         return;
       }
@@ -179,8 +168,92 @@ const addPost = (req, res) => {
     res.send({ id: newPost._id });
   };
 
-  const data = new PostModel(payload);
-  data.save(onPostSave);
+  const setThumbHelper = (req, payload) => {
+    if (!req.body.thumbnailURL){
+      let videoThumb = "5f736f876b93c95300af61e0";
+      let audioThumb = "5f736fae6b93c95300af61e1";
+      let imageThumb = "5f736fdd6b93c95300af61e2";
+      let textThumb = "5f7370026b93c95300af61e3";
+      let applicationThumb = "5f7370156b93c95300af61e4";
+      switch (payload.contentCategory) {
+        case "video":
+          payload.thumbnailURL = videoThumb;
+          break;
+        case "image":
+          payload.thumbnailURL = imageThumb;
+          break;
+        case "audio":
+          payload.thumbnailURL = audioThumb;
+          break;
+        case "text":
+          payload.thumbnailURL = textThumb;
+          break;
+        case "application":
+          payload.thumbnailURL = applicationThumb;
+          break;
+      }
+    }
+    else{
+      payload.thumbnailURL = req.body.thumbnailURL;
+    }
+
+    if (req.body.private) {
+      payload.private = req.body.private;
+    }
+
+    return payload;
+  }
+
+  if (!req.body.title){
+    console.log("addPost not successful: missing post title");
+    res.status(400);
+    res.send("Add post not successful - missing post attributes");
+    return;
+  }
+  if (!req.body.mediaID){
+    console.log("addPost not successful: missing post media");
+    res.status(400);
+    res.send("Add post not successful - missing post media");
+  }
+
+  Media.findById(req.body.mediaID)
+      .lean()
+      .then((doc) => {
+        console.log("checking if user has access to media");
+        if (
+            doc.creator.toString() !== req.user.id.toString() &&
+            doc.isPrivate !== false &&
+            !doc.canAccess.includes(mongoose.Types.ObjectId(req.user.id))
+        ) {
+          console.log("Poster does not have access permissions for media in their post\"");
+          sendHelper(res, {
+            status: 401,
+            msg:
+                "Add post was not successful - user does not have access permissions for media in their post",
+          });
+          return;
+        }else{
+          //build payload and set content category
+          const payload = { title: req.body.title, userID: req.user.id, mediaID: req.body.mediaID};
+          if (req.body.description) {
+            payload.description = req.body.description;
+          }
+          payload.contentCategory = doc.contentCategory;
+          setThumbHelper(req, payload);
+          console.log("Finished building payload");
+          const data = new PostModel(payload);
+          data.save(onPostSave);
+
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        sendHelper(res, {
+          status: 503,
+          msg:
+              "Add post was not successful - error checking media information in database",
+        });
+      });
 };
 
 const updatePost = (req, res) => {

e79de1623bbee71e55b3af96117794739816c1d2
arrikenw <arrikenw@student.unimelb.edu.au>
2020-10-02 13:24:33 +1000

    fixed setting back button

--- client/src/components/Settings/Settings.js
+++ client/src/components/Settings/Settings.js
@@ -9,19 +9,19 @@ class Settings extends Component {
 
     render() {
         return (
-            <div>
-                <div style={{marginTop: "5vh", marginLeft: "15vw", marginRight:"15vw" , backgroundColor: "#d3d6d6"}}>
-                    <h3 style={{marginTop:"2vh",marginLeft:"2vw"}}>Settings</h3>
-                    <br/>
+          <div>
+            <div style={{marginTop: "5vh", marginLeft: "15vw", marginRight:"15vw" , backgroundColor: "#d3d6d6"}}>
+              <h3 style={{marginTop:"2vh",marginLeft:"2vw"}}>Settings</h3>
+              <br />
 
-                    <SettingsForm setToken = {this.props.setToken}/>
-                    <br></br>
-                </div>
-
-                <Button href={"/profilepage"} style = {{float: "right", marginRight:"15vw"}}>
-                    <div>Back</div>
-                </Button>
+              <SettingsForm setToken={this.props.setToken} />
+              <br />
             </div>
+
+            <Button href={`/profile?user=${this.props.user._id}`} style={{float: "right", marginRight:"15vw"}}>
+              <div>Back</div>
+            </Button>
+          </div>
         )
     }
 }

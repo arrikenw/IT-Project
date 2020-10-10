2986fe01ed4bf613cf20b2146cb64abef8a43036
jfkenna-unimelb <jfkenna@student.unimelb.edu.au>
2020-10-10 12:14:54 +1100

    Pinned posts w/ material UI.
    + Rewrote pinned posts in a material-UI style.
    + Changed pinned posts to fetch posts recursively
    + Changed pinned posts so that posts are stored rather than refetched

---
+++ client/src/components/profile/PinnedPosts.js
@@ -0,0 +1,146 @@
+import React, {Component} from "react";
+import Axios from "axios";
+import {Button} from "@material-ui/core";
+import { Grid } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import ProfilePost from "./ProfilePost";
+
+
+class PinnedPost extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {ids: [], i: 0, posts: []}
+        this.leftScroll = this.leftScroll.bind(this);
+        this.rightScroll = this.rightScroll.bind(this);
+        this.getPinnedPostContent = this.getPinnedPostContent.bind(this);
+    }
+
+    leftScroll = (magnitude) => {
+        let newi = this.state.i - magnitude;
+        if (newi < 0){
+            newi = 0;
+        }
+        this.setState({i: newi});
+    }
+
+    rightScroll = (magnitude) => {
+        const newi = this.state.i + magnitude;
+        this.setState({i: newi});
+    }
+
+    getPinnedPostContent = (postids, exportPosts, errcount) => {
+        if (postids.length == 0){
+            //return once we have processed all posts
+            this.setState({posts: exportPosts});
+            return;
+        }
+        console.log("trying id: "+postids[0]);
+
+        //recursively fetch first element and shorten array
+        const headers = {
+            headers: { 'Authorization': 'Bearer '+ window.localStorage.getItem("token")}
+        }
+        let postUrl = '/api/post/get'
+        const postPayload = {
+            filters: {_id: postids[0]}
+        }
+
+        Axios.post(postUrl, postPayload, headers).then( async (res) => {
+            postids.shift(); //pop off first element
+            if (res.status == 200 || res.status == "success"){
+                console.log("res: ");
+                console.log(res.data);
+                console.log(res.data[0]);
+                exportPosts.push(res.data[0]);
+                this.getPinnedPostContent(postids, exportPosts, errcount);
+            }else{
+                //TODO Look into this
+                //idk if we should add an error post or just ignore. continuing recursion for now
+                this.getPinnedPostContent(postids, exportPosts, errcount);
+            }
+        }).catch((err) => {
+            //TODO look into this
+            //idk if we should add an error post or just ignore. continuing recursion for now
+
+            //stop attempting after 3 failures
+            if (errcount < 3) {
+                this.getPinnedPostContent(postids, exportPosts, errcount + 1);
+            }
+        });
+    }
+
+    // TODO add second request if user is requesting themselves rather than getPublicUser
+    componentDidMount() {
+        const controllerUrl = "/api/user/getPublic";
+        const payload = {
+            ids: [this.props.id]
+        }
+        Axios({url: controllerUrl, method: "post", data: payload})
+            .then((res) => {
+                if (res.status == 200 || res.status == "success") {
+                    this.setState({ids: res.data[0].pinnedPosts});
+                    console.log("PINNED POST IDS");
+                    console.log(res.data[0].pinnedPosts);
+                    this.getPinnedPostContent(res.data[0].pinnedPosts, [], 0);
+                }
+            })
+            .catch((err) => {
+                console.log("GET USER FAILED WITH ERR")
+                console.log(err);
+                // todo;
+            });
+    }
+
+
+    // TODO REWRITE SO THAT THE POSTS ARE STORED AND ONLY REREQUESTED IF REQUIRED
+    render(){
+        if (this.state.ids){
+            console.log("ids");
+            console.log(this.state.ids.length);
+            console.log(this.state.ids[0]);
+            console.log(this.state.ids);
+            console.log("posts:")
+            console.log(this.state.posts);
+            console.log("/");
+            /*
+            if (this.state.ids.length === 0) {
+                return (
+                    <div> nothing </div>)
+            }*/
+
+            return (
+                <Grid container>
+                    <Grid container>
+                        <Grid item xs={4}>
+                            <div>
+                                {(this.state.i < this.state.posts.length) && (<ProfilePost post={this.state.posts[this.state.i+0]} />)}
+                            </div>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <div>
+                                {(this.state.i + 1 < this.state.posts.length) && (<ProfilePost post={this.state.posts[this.state.i+1]} />)}
+                            </div>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <div>
+                                {(this.state.i + 2 < this.state.posts.length) && (<ProfilePost post={this.state.posts[this.state.i+2]} />)}
+                            </div>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={11}>
+                        <Button style={{float:"left"}}onClick={()=> {this.leftScroll(1);}}> LEFT </Button>
+                    </Grid>
+                    <Grid item xs={1}>
+                        <Button style={{float:"right"}} onClick={()=> {this.rightScroll(1);}}> RIGHT </Button>
+                    </Grid>
+                </Grid>)
+
+
+        }
+        return <div> Sorry, no posts have been pinned yet.</div>
+
+    }
+}
+
+export default PinnedPost;
--- client/src/components/profile/ProfilePost.js
+++ client/src/components/profile/ProfilePost.js
@@ -1,45 +1,100 @@
-import React from 'react'
+import Axios from "axios";
+import React, {Component} from "react";
+import { withStyles } from "@material-ui/core/styles";
 import {
   Card,
+  CardMedia,
   CardActionArea,
   CardContent,
   Button,
   Typography,
   CardActions,
 } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 
-const useStyles = makeStyles({
+//https://stackoverflow.com/questions/50272814/image-on-material-ui-cardmedia
+const styles = theme => ({
   postCard: {
     height: '350px',
     marginBottom: '30px',
   },
-})
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9,
+    marginTop:'30'
+  }
+});
 
-function ProfilePost({ post }) {
-  const classes = useStyles()
-  return (
-    <Card className={classes.postCard}>
-      <CardActionArea>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {post.title}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {post.description}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
-        </Button>
-      </CardActions>
-    </Card>
-  )
+
+class ProfilePost extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      mimeType: "",
+      contentStr: "",
+      contentCategory: "",
+    };
+  }
+
+
+  componentDidMount() {
+    if (this.props.post.mediaID === "") {
+      return;
+    }
+
+    const controllerUrl = "/api/media/";
+    const payload = {
+      mediaID: this.props.post.mediaID,
+    };
+    const headers = {
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+    };
+    Axios.post(controllerUrl, payload, headers)
+        .then((res) => {
+          if (res.status === 200) {
+            const str = `data:${res.data.mimeType};base64,${res.data.b64media}`;
+            this.setState({
+              contentStr: str,
+              mimeType: res.data.mimeType,
+              contentCategory: res.data.contentCategory,
+            });
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          // todo;
+        });
+  }
+
+  render(){
+    const classes = this.props.classes;
+    console.log("media:")
+    console.log(this.state.contentStr);
+    return (
+        <Card className={classes.postCard}>
+          <CardActionArea>
+            <CardContent>
+              <CardMedia square  className={classes.media} src={this.state.contentStr}></CardMedia>
+              <Typography gutterBottom variant="h5" component="h2">
+                {this.props.post.title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                {this.props.post.description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+    )
+  }
 }
-
-export default ProfilePost
+export default withStyles(styles, { withTheme: true })(ProfilePost)
